9578453db2ec43c57c49ffa918ccfc26061b583d
Xiaoxi Guo <xguo0122@gmail.com>
2015-12-06 13:31:04 -0600

    add choose rest and camera activity

---
+++ app/src/main/java/com/xiaoxiguo/fastfoodrecog/ChooseRestActivity.java
@@ -0,0 +1,110 @@
+package com.xiaoxiguo.fastfoodrecog;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ChooseRestActivity extends AppCompatActivity {
+
+    private RadioGroup restList;
+    private int restId = -1;
+
+    private static final int MEDIA_TYPE_IMAGE = 1;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    private Uri fileUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.choose_rest_title);
+        setContentView(R.layout.activity_choose_rest);
+
+        restList = (RadioGroup) findViewById(R.id.restList);
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (restId == -1) {
+                    Toast.makeText(ChooseRestActivity.this, "Please select one restaurant!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.v("Procedure", "Confirmed choosing restaurant " + restId);
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    // create a file to save the image
+                    try {
+                        fileUri = Uri.fromFile(createImageFile(MEDIA_TYPE_IMAGE));
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                        // start the image capture Intent
+                        startActivityForResult(cameraIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                    } catch (IOException e) {
+                        Toast.makeText(ChooseRestActivity.this, "No space to save image.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    public void onRadioButtonClicked(View v) {
+        restId = restList.getCheckedRadioButtonId();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bundle bundle = new Bundle();
+                bundle.putString("filepath", fileUri.getPath());
+                Intent imageIntent = new Intent(ChooseRestActivity.this, ImageActivity.class);
+                imageIntent.putExtras(bundle);
+                startActivity(imageIntent);
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+                Toast.makeText(this, "Image capture cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                // Image capture failed, advise user
+                Toast.makeText(this, "Image capture failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    /**
+     * Create a File for saving an image
+     */
+    private File createImageFile(int type) throws IOException {
+        // Create a dir to save files it not exists.
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "FastFoodRecognizer");
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                throw new IOException("Failed to create directory");
+            }
+        }
+        Log.v("Procedure", "Image will be saved to " + mediaStorageDir.getPath());
+
+        // Create a file for image
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            String filename = mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".png";
+            mediaFile = new File(filename);
+        } else {
+            throw new IOException("Failed to create image file");
+        }
+        Log.v("Procedure", "Image will be saved as " + mediaFile.getName());
+        return mediaFile;
+    }
+}
--- app/src/main/java/com/xiaoxiguo/fastfoodrecog/MainActivity.java
+++ app/src/main/java/com/xiaoxiguo/fastfoodrecog/MainActivity.java
@@ -1,8 +1,13 @@
 package com.xiaoxiguo.fastfoodrecog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import org.opencv.core.Core;
 
@@ -10,12 +15,40 @@ public class MainActivity extends AppCompatActivity {
 
     static {
         System.loadLibrary("opencv_java");
+        Log.i("opencv", "Loaded opencv " + Core.VERSION);
     }
 
+    /**
+     * Created by Harris.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("opencv", "Load opencv " + Core.VERSION);
+        Log.d("MainActivity", "onCreate");
+
+        // Listen to connect to choose restaurant
+        Button restButton = (Button) findViewById(R.id.choose_rest_button);
+        restButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "You clicked Restaurant", Toast.LENGTH_SHORT).show();
+                Intent secIntent = new Intent(MainActivity.this, ChooseRestActivity.class);
+                startActivity(secIntent);
+            }
+        });
+
+        // Listen to connect to camera
+        Button cameraButton = (Button) findViewById(R.id.camera_button);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.hint_text, Toast.LENGTH_SHORT).show();
+                // create imageIntent to take a photo
+                Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(imageIntent, 0);
+            }
+        });
     }
 }
